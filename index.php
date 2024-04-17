070289cd44a82986d6a86435d837f12c65bb356c
Luke Cheng <lukeecheng@gmail.com>
2024-04-17 15:53:46 -0400

    Added get & post routing function

--- index.php
+++ index.php
@@ -50,5 +50,43 @@ $f3->route('GET /menus/dinner', function(){
     echo $view->render('views/dinner-menu.html');
 });
 
+//Order 1
+$f3->route('GET|POST /order1', function($f3){
+//    echo '<h1>My dinner menus</h1>';
+
+    //If the form has been posted
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+//        echo "<p> You got here using the POST method </p>";
+//        var_dump($_POST);
+
+        //Get the data from the post array
+        $food = $_POST['food'];
+        $meal = $_POST['meal'];
+
+        //If the data is valid
+        if(true){
+            $f3->set('SESSION.food', $food);
+            $f3->set('SESSION.meal', $meal);
+
+        }
+    }
+    else{
+        echo "<p> You got here using the GET method </p>";
+    }
+
+//    //Render a view page
+    $view = new Template();
+    echo $view->render('views/order1.html');
+});
+
+//Order 2
+$f3->route('GET /order2', function(){
+//    echo '<h1>My dinner menus</h1>';
+
+//    //Render a view page
+    $view = new Template();
+    echo $view->render('views/order2.html');
+});
+
 //Run Fat-Free
 $f3->run();
